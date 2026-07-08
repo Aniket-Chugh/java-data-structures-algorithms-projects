8fd3c59494bf957451fa80ee6927deff0f6539ad
Aniket Chugh <oneaniket13@gmail.com>
2026-07-08 11:29:14 +0530

    first commit

---
+++ project-1/to-dolist/src/model/Tasks.java
@@ -0,0 +1,17 @@
+package model;
+
+public class Tasks {
+    private int yoyo;
+
+    public void setyoyo(int text) {
+        yoyo = text;
+    }
+
+    public int getyoyo() {
+        return yoyo;
+    }
+
+    public String message() {
+        return "yoyo is a function calling string";
+    }
+}
